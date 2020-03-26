a6fd3f6117a637fecd86ee34fab86c73c4544d87
Andre <andrempmattos@gmail.com>
2020-03-26 22:54:37 +0100

    FPGA_SoC: Exporting M2S010 device firmware drivers and configurations

---
+++ fpga_soc/firmware/README.txt
@@ -0,0 +1,6 @@
+Export Firmware README
+
+Microsemi Corporation - Microsemi Libero Software Release v11.8 (Version 11.8.0.26)
+
+Date    :    Thu Mar 26 22:50:38 2020
+Project :    C:\Users\lirmm2\Desktop\GitHub\HARSH\fpga_soc
---
+++ fpga_soc/firmware/top_hw_platform.h
@@ -0,0 +1,16 @@
+#ifndef top_HW_PLATFORM_H_
+#define top_HW_PLATFORM_H_
+/*****************************************************************************
+*
+*Created by Microsemi SmartDesign  Thu Mar 26 22:50:37 2020
+*
+*Memory map specification for peripherals in top
+*/
+
+/*-----------------------------------------------------------------------------
+* CM3 subsystem memory map
+* Master(s) for this subsystem: CM3 
+*---------------------------------------------------------------------------*/
+
+
+#endif /* top_HW_PLATFORM_H_*/
