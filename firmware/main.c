60fc00bbb295eca8b953b687611589d50ad7ae55
Andre <andrempmattos@gmail.com>
2020-03-27 11:23:34 +0100

    firmware: Adding code style patterns and peripherals initialization

--- firmware/main.c
+++ firmware/main.c
@@ -1,68 +1,91 @@
-/*******************************************************************************
- * (c) Copyright 2010-2015 Microsemi SoC Products Group.  All rights reserved.
+/* 
+ * main.c
  *
- * High Performance DMA transfer example
+ * MIT License
  *
- * Please refer to the file README.txt for further details about this example.
+ * Copyright (c) 2020 André Mattos
  *
- * SVN $Revision: 7926 $
- * SVN $Date: 2015-10-01 14:59:10 +0530 (Thu, 01 Oct 2015) $
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ *
  */
+
+/**
+ * \brief Main file.
+ * 
+ * \author Andre Mattos <andrempmattos@gmail.com>
+ * 
+ * \version 0.0.1
+ * 
+ * \date 27/03/2020
+ * 
+ * \defgroup main Main file
+ * \{
+ */
+
+
 #include "drivers/mss_hpdma/mss_hpdma.h"
+#include "drivers/mss_gpio/mss_gpio.h"
+#include "drivers/mss_uart/mss_uart.h"
+#include "CMSIS/system_m2sxxx.h"
+#include "top_hw_platform.h"
 
-/*==============================================================================
-  Constants.
+#define HPDMA_TRANSFER_SIZE             16
+#define HPDMA_TRANSFER_SIZE_ARRAY       ( HPDMA_TRANSFER_SIZE/4 )
+
+/*
+ * Local functions prototypes.
  */
-#define HPDMA_TRANSFER_SIZE     1024
+static void peripherals_init (void);
+static void delay(void);
 
-#define TARGET_MDDR_ADDRESS     0xA0000000u
+static void xfer_complete_handler (hpdma_status_t status);
+static void load_pattern (uint32_t * buffer, uint32_t word_size);
 
-/*==============================================================================
-  Global variables.
+static void uart_splash_message(void);
+
+
+/*
+ * The g_buffer_a and g_buffer_b arrays are used for HPDMA transfers.
  */
-/*------------------------------------------------------------------------------
- * The g_buffer_a array is used as the source of PDMA transfers.
- */
-uint32_t g_buffer_a[HPDMA_TRANSFER_SIZE];
+uint32_t g_buffer_a[HPDMA_TRANSFER_SIZE_ARRAY];
+uint32_t g_buffer_b[HPDMA_TRANSFER_SIZE_ARRAY];
 
-/*------------------------------------------------------------------------------
- * The g_buffer_b array is used as destination of the PDMA transfers.
- */
-uint32_t g_buffer_b[HPDMA_TRANSFER_SIZE];
-
-/*------------------------------------------------------------------------------
+/*
  * Transfer complete Status updated by Interrupt routine.
  */
 volatile uint8_t  g_xfr_completed;
 
-/*==============================================================================
-  Local functions.
+/*
+ * Instance of UART0 to be used by the driver operations
  */
-static void xfer_complete_handler
-(
-    hpdma_status_t status
-);
+mss_uart_instance_t * const gp_my_uart = &g_mss_uart0;
 
-static void load_pattern
-(
-    uint32_t * buffer,
-    uint32_t word_size
-);
 
-/*==============================================================================
+/*
  * main function.
  */
 int main(void)
 {
-    /*
-     *  Initialize HPDMA controller.
-     */
-    MSS_HPDMA_init();
 
     /* Infinite loop */
-    for(;;)
+    while(1)
     {
-        volatile uint32_t dummy;
 
         /*
          * Load pattern into g_buffer_a word array.
@@ -72,56 +95,66 @@ int main(void)
         MSS_HPDMA_set_handler(xfer_complete_handler);
 
         /*
-         * Start a HPDMA transfer from g_buffer_a[] to g_buffer_b.
+         * Start a HPDMA transfer from g_buffer_a[] to SDRAM and wait for the operation on HPDMA channel 0 to complete.
          */
         g_xfr_completed = 0u;
-        MSS_HPDMA_start((uint32_t)g_buffer_a,
-                        TARGET_MDDR_ADDRESS,
-                        HPDMA_TRANSFER_SIZE,
-                        HPDMA_TO_DDR);
+        MSS_HPDMA_start((uint32_t)g_buffer_a, EXTERNAL_SDR_SDRAM_ADDR, HPDMA_TRANSFER_SIZE, HPDMA_TO_DDR);
+        while(0u == g_xfr_completed);
 
         /*
-         * Wait for the transfer on HPDMA channel 0 to complete.
-         */
-         while(0u == g_xfr_completed)
-         {
-            ;
-         }
-
-        /*
-         * Start a HPDMA transfer from g_buffer_a[] to g_buffer_b.
+         * Start a HPDMA transfer from SDRAM to g_buffer_b[] and wait for the operation on HPDMA channel 0 to complete.
          */
         g_xfr_completed = 0u;
-        MSS_HPDMA_start(TARGET_MDDR_ADDRESS,
-                        (uint32_t)g_buffer_b,
-                        HPDMA_TRANSFER_SIZE,
-                        HPDMA_FROM_DDR);
+        MSS_HPDMA_start(EXTERNAL_SDR_SDRAM_ADDR, (uint32_t)g_buffer_b, HPDMA_TRANSFER_SIZE, HPDMA_FROM_DDR);
+        while(0u == g_xfr_completed);
 
-        /*
-         * Wait for the transfer on HPDMA channel 0 to complete.
-         */
-         while(0u == g_xfr_completed)
-         {
-            ;
-         }
 
-        /*
-         * Set a breakpoint on the next line. Adding a watch on the g_buffer_b
-         * buffer will show the the content of the buffer change as the HPDMA
-         * transfers take place from g_buffer_a to g_buffer_b.
-         */
-        ++dummy;
     }
 }
-/*==============================================================================
+
+
+
+/*
+ * Peripherals initialization.
+ */
+static void peripherals_init(void)
+{
+    /*
+     *  Initialize HPDMA controller.
+     */
+    MSS_HPDMA_init();
+
+    /*
+     *  Initialize UART0 interface.
+     */
+    MSS_UART_init(gp_my_uart, MSS_UART_115200_BAUD, MSS_UART_DATA_8_BITS | MSS_UART_NO_PARITY | MSS_UART_ONE_STOP_BIT);
+
+    /*
+     *  Initialize GPIO ports.
+     */
+    MSS_GPIO_init();
+    MSS_GPIO_config(MSS_GPIO_29, MSS_GPIO_INPUT_MODE);
+    MSS_GPIO_config(MSS_GPIO_25, MSS_GPIO_OUTPUT_MODE);
+}
+
+/*
+  Primitive delay operation between displays of the watchdog counter value.
+ */
+static void delay(void)
+{
+    volatile uint32_t delay_count = SystemCoreClock / 128u;
+
+    while(delay_count > 0u)
+    {
+        --delay_count;
+    }
+}
+
+/*
  * Load bit pattern into buffer passed as parameter. The pattern will change
  * with each call to this function.
  */
-static void load_pattern
-(
-    uint32_t * buffer,
-    uint32_t word_size
-)
+static void load_pattern(uint32_t * buffer, uint32_t word_size)
 {
     uint32_t i;
     static uint32_t increment = 1;
@@ -132,14 +165,26 @@ static void load_pattern
     ++increment;
 }
 
-/*==============================================================================
+/*
  * HPDMA transfer complete handler.
  */
-static void xfer_complete_handler
-(
-    hpdma_status_t status
-)
+static void xfer_complete_handler(hpdma_status_t status)
 {
     /* Transfer has completed */
     g_xfr_completed = 1u;
 }
+
+/*
+ * UART splash message.
+ */
+static void uart_splash_message(void)
+{
+    MSS_UART_polled_tx_string(gp_my_uart,(const uint8_t*)"\n\r\n\r**********************************************************************\n\r");
+    MSS_UART_polled_tx_string(gp_my_uart,(const uint8_t*)"************************** Prototype Demo ****************************\n\r");
+    MSS_UART_polled_tx_string(gp_my_uart,(const uint8_t*)"**********************************************************************\n\r");
+    MSS_UART_polled_tx_string(gp_my_uart,(const uint8_t*)"************* Demonstrate SDRAM, UART and GPIO operation *************\n\r");
+    MSS_UART_polled_tx_string(gp_my_uart,(const uint8_t*)"**********************************************************************\n\r");
+}
+
+
+/** \} End of main group */
