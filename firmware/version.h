8ddfe06b3c8d6ad3a52ce85fbb9c01adff72b446
Andre <andrempmattos@gmail.com>
2020-05-08 18:32:32 +0200

    firmware: version: Adding version control file and changelog

---
+++ firmware/version.h
@@ -0,0 +1,54 @@
+/*
+ * version.h
+ *
+ * MIT License
+ *
+ * Copyright (c) 2020 André Mattos
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ *
+ */
+
+/**
+ * \brief Version control file.
+ * 
+ * \author Andre Mattos <andrempmattos@gmail.com>
+ * 
+ * \version 0.0.1
+ * 
+ * \date 08/05/2020
+ * 
+ * \defgroup version Version
+ * \{
+ */
+
+#ifndef VERSION_H_
+#define VERSION_H_
+
+#define FIRMWARE_VERSION            "0.0.1"
+
+#define FIRMWARE_STATUS             "Development"
+
+#define FIRMWARE_AUTHOR             "Andre Mattos"
+
+#define FIRMWARE_AUTHOR_EMAIL       "andre.mattos@spacelab.ufsc.br"
+
+#endif /* VERSION_H_ */
+
+/** \} End of version group */
