2b1f953959ec7b6c48a43ec5c41679258756ab3a
Andre <andrempmattos@gmail.com>
2020-05-08 18:34:35 +0200

    firmware: project: Updating the project folders structure

---
+++ firmware/drivers/drivers.h
@@ -0,0 +1,58 @@
+/*
+ * drivers.h
+ *
+ * MIT License
+ *
+ * Copyright (c) 2020 André Mattos
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ *
+ */
+
+/**
+ * \brief Drivers layer includes.
+ * 
+ * \author Andre Mattos <andrempmattos@gmail.com>
+ * 
+ * \version 0.0.1
+ * 
+ * \date 08/05/2020
+ * 
+ * \defgroup drivers Drivers
+ * \{
+ */
+
+#ifndef DRIVERS_H_
+#define DRIVERS_H_
+
+#include "mss_can/mss_can.h"
+#include "mss_gpio/mss_gpio.h"
+#include "mss_hpdma/mss_hpdma.h"
+#include "mss_i2c/mss_i2c.h"
+#include "mss_nvm/mss_nvm.h"
+#include "mss_spi/mss_spi.h"
+#include "mss_sys_services/mss_sys_services.h"
+#include "mss_timer/mss_timer.h"
+#include "mss_uart/mss_uart.h"
+#include "mss_watchdog/mss_watchdog.h"
+
+
+#endif /* DRIVERS_H_ */
+
+/** \} End of drivers group */
