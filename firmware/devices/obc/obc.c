313c11fc7a9f878c90965049f8e697921be813d9
Andre <andrempmattos@gmail.com>
2020-05-12 14:44:13 +0200

    firmware: devices: obc: Adding the prototype implementation

---
+++ firmware/devices/obc/obc.c
@@ -0,0 +1,69 @@
+/*
+ * obc.c
+ *
+ * MIT License
+ *
+ * Copyright (c) 2020 André Mattos
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ *
+ */
+
+/**
+ * \brief OBC interface implementation.
+ * 
+ * \author Andre Mattos <andrempmattos@gmail.com>
+ * 
+ * \version 0.0.7
+ * 
+ * \date 12/05/2020
+ * 
+ * \addtogroup obc
+ * \{
+ */
+
+#include <system/sys_log/sys_log.h>
+#include <drivers/mss_i2c/mss_i2c.h>
+#include <drivers/mss_spi/mss_spi.h>
+#include <drivers/mss_can/mss_can.h>
+#include <drivers/mss_uart/mss_uart.h>
+
+#include "leds.h"
+
+int obc_init(void) 
+{
+
+}
+
+int obc_get_command(obc_command_t *command)
+{
+
+}
+
+int obc_send_data(obc_data_t *data)
+{
+
+}
+
+int obc_send_state(system_state_t *state)
+{
+
+}
+
+/** \} End of obc group */
---
+++ firmware/devices/obc/obc.h
@@ -0,0 +1,149 @@
+/*
+ * leds.h
+ *
+ * MIT License
+ *
+ * Copyright (c) 2020 André Mattos
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ *
+ */
+
+/**
+ * \brief OBC interface definition.
+ * 
+ * \author Andre Mattos <andrempmattos@gmail.com>
+ * 
+ * \version 0.0.7
+ * 
+ * \date 12/05/2020
+ * 
+ * \defgroup obc OBC
+ * \ingroup devices
+ * \{
+ */
+
+#ifndef OBC_H_
+#define OBC_H_
+
+#define LEDS_MODULE_NAME        		"OBC"
+
+/* Operation modes */
+#define EXPERIMENT_PAYLOAD          	0       /**< Experiment payload operation mode. */
+#define HEALTH_MONITOR              	1       /**< Health monitor operation mode. */
+
+/* Execution configurations */
+#define ENABLE_SDRAM_MEMORY_B       	(1 << 0)       /**< Enable the SDRAM memory B. */
+#define ENABLE_SDRAM_MEMORY_D       	(1 << 1)       /**< Enable the SDRAM memory D. */
+#define ENABLE_SDRAM_MEMORY_F       	(1 << 2)       /**< Enable the SDRAM memory F. */
+#define ENABLE_STATIC_TESTS         	(1 << 3)       /**< Enable static algorithm tests. */
+#define ENABLE_DYNAMIC_TESTS        	(1 << 4)       /**< Enable dynamic algorithm tests. */
+#define ENABLE_FREQUENCY_TESTS      	(1 << 5)       /**< Enable frequency variation tests. */
+#define ENABLE_REFRESH_RATE_TESTS   	(1 << 6)       /**< Enable refresh rate variation tests. */
+#define FORCE_LOW_POWER      			(1 << 7)       /**< Force low power execution. */
+#define FORCE_REDUCED_LOG      			(1 << 8)       /**< Force reduced log report. */
+#define FORCE_HALT_AFTER_LATCHUP		(1 << 9)       /**< Force system halt after latchup. */
+
+/* Error codes */
+#define BOOT_FAILURE     				(1 << 0)       /**< Initialization error. */
+#define SYSTEM_FAILURE     				(1 << 1)       /**< System error. */
+#define EXPERIMENT_FAILURE     			(1 << 2)       /**< Experiment execution error. */
+#define MEMORY_B_INTERFACE_ERROR		(1 << 3)       /**< SDRAM memory B interface error. */
+#define MEMORY_D_INTERFACE_ERROR       	(1 << 4)       /**< SDRAM memory D interface error. */
+#define MEMORY_F_INTERFACE_ERROR        (1 << 5)       /**< SDRAM memory F interface error. */
+#define MEMORY_B_LATCHUP        		(1 << 6)       /**< SDRAM memory B latchup detection. */
+#define MEMORY_D_LATCHUP      			(1 << 7)       /**< SDRAM memory D latchup detection. */
+#define MEMORY_F_LATCHUP      			(1 << 8)       /**< SDRAM memory F latchup detection. */
+
+/**
+ * \brief OBC commands.
+ */
+typedef struct
+{
+    uint8_t operation_mode;				/**< Operation mode selection. */
+    uint16_t execution_config;			/**< Execution configuration. */
+    uint32_t obc_sys_time;				/**< OBC system time syncronization. */
+} obc_command_t;
+
+/**
+ * \brief OBC data.
+ */
+typedef struct
+{
+	uint32_t time_stamp;				/**< Data time stamp. */
+	uint8_t device;						/**< Device in test. */
+	uint8_t memory_frequency;			/**< Memory frequency during this test (in MHz). */
+	uint16_t refresh_rate;				/**< Memory refresh rate during this test (cycles/ms). */
+    uint8_t algorithm;         			/**< Algorithm executed. */
+    uint16_t iteration;         		/**< Iteration cycle in dynamic tests. */
+    uint32_t address;	         		/**< Address with detected error. */
+    uint32_t data;         				/**< Data in the malfunctioning address. */
+    uint16_t error;         			/**< Error code. */
+} obc_data_t;
+
+/**
+ * \brief system state.
+ */
+typedef struct
+{
+	uint32_t time_stamp;				/**< Current time stamp. */
+	uint8_t operation_mode;				/**< Current operation mode. */	
+	uint16_t execution_config;			/**< Current execution configuration. */
+	uint8_t error_count;				/**< Errors count. */
+	uint16_t error_code;				/**< Error code. */
+} system_state_t;
+
+/**
+ * \brief Initialization routine of the OBC interface.
+ *
+ * \return The status/error code.
+ */
+int obc_init(void);
+
+/**
+ * \brief Gets commands from OBC.
+ *
+ * \param[in] command is a pointer store the obc command.
+ *
+ * \return The status/error code.
+ */
+int obc_get_command(obc_command_t *command);
+
+/**
+ * \brief Gets commands from OBC.
+ *
+ * \param[in] command is a pointer store the obc data package.
+ *
+ * \return The status/error code.
+ */
+int obc_send_data(obc_data_t *data);
+
+/**
+ * \brief Gets the general state of the payload.
+ *
+ * \param[in] command is a pointer store the obc data package.
+ *
+ * \return The status/error code.
+ */
+int obc_send_state(system_state_t *state);
+
+
+#endif /* OBC_H_ */
+
+/** \} End of obc group */
