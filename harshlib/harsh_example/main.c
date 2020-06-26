44f2e9564cd4801a1f6d0950cb63ca14daf57886
Andre <andrempmattos@gmail.com>
2020-06-26 11:41:04 +0200

    harshlib: example: main: Adding preliminary implementation

--- harshlib/harsh_example/main.c
+++ harshlib/harsh_example/main.c
@@ -30,7 +30,7 @@
  * 
  * \author Andre Mattos <andrempmattos@gmail.com>
  * 
- * \version 0.0.0
+ * \version 0.0.2
  * 
  * \date 23/06/2020
  * 
@@ -40,5 +40,127 @@
 
 #include "../harshlib/harshlib.h"
 
+#define EXPERIMENT_SECTION_PERIOD	(10*1000)		/* Period between experiment section cycles */
+#define EXPERIMENT_ROUTINE_PERIOD	(10*1000)		/* Period between experiment routine cycles */
+#define EXPERIMENT_ROUTINE_CYCLES	60				/* Number of routine cycles performed in one experiment section */
+#define MAX_PAYLOAD_ERRORS_ALLOWED	20				/* Maximum payload errors allowed */
+#define MAX_PAYLOAD_DATA_PACKAGES	5				/* Maximum payload errors allowed */
+
+int main(int argc, char const *argv[])
+{
+	command_package_t command;
+	state_package_t state;
+	data_package_t data;
+
+	/* GPIO initialization routine (used for the payload enables) */
+	gpio_init();
+
+	/* SPI initialization routine (used for the payload communication) */
+	spi_init();
+
+	/* GPIO initialization routine (keep the paylaod turned-off) */
+	harsh_init();
+
+	/* This loop perform periodic experiment sections (it could be scheduled instead) */
+	while(1) 
+	{
+		/* Turn-on the payload and perform a interface communication test */
+		if(harsh_start() != 0)
+		{
+			log_print_event("Error detected during initialization!");
+		}
+
+		/* Set command parameters */
+		command.operation_mode = EXPERIMENT_PAYLOAD;
+		command.execution_config = ENABLE_SDRAM_MEMORY_B | ENABLE_SDRAM_MEMORY_D | ENABLE_SDRAM_MEMORY_F | ENABLE_STATIC_TESTS;
+		command.obc_sys_time = get_timestamp();
+
+		if(harsh_set_config(command) != 0)
+		{
+			log_print_event("Error detected during set config routine!");
+		}
+
+		/* This loop perform the regular routines during an experiment section */
+		while(cycle++ < EXPERIMENT_ROUTINE_CYCLES) 
+		{
+			/* Get the payload state */
+			if(harsh_get_state(state) == 0)
+			{
+				/* Check timestamp synchronization */
+				if(state.time_stamp != get_timestamp()) 
+				{
+					log_print_event("Different timestamp detected!");
+				}
+
+				/* Check parameters configuration */
+				if((state.operation_mode != command.operation_mode) || (state.execution_config != command.execution_config))
+				{
+					log_print_event("Different parameter config detected!");	
+				}
+
+				/* Check errors (it could be implement as an error handler instead) */
+				if(state.error_count < MAX_PAYLOAD_ERRORS_ALLOWED)
+				{
+					/* Case experiment critical failure, then stop the execution for this entire section */
+					if (state.error_code & EXPERIMENT_FAILURE)
+					{
+						harsh_stop();
+						log_print_event("Payload turned-off!");
+					}
+					/* Case experiment memory interface failure, then just notify (it should be properly handled) */ 
+					if (state.error_code & (MEMORY_B_INTERFACE_ERROR | MEMORY_D_INTERFACE_ERROR | MEMORY_F_INTERFACE_ERROR))
+					{
+						log_print_event("Experiment memories interface errors detected");
+					}
+					/* Case experiment memory latch-up failure, then just notify (it should be properly handled) */
+					if (state.error_code & (MEMORY_B_LATCHUP | MEMORY_D_LATCHUP | MEMORY_F_LATCHUP))
+					{
+						log_print_event("Experiment memories latch-up errors detected");	
+					}
+				}
+				else 
+				{
+					/* Case maximum allowed errors achieved, then stop execution for this entire section */
+					harsh_stop();
+					log_print_event("Maximum payload allowed errors achieved");
+					log_print_event("Payload turned-off!");
+				}
+
+				/* Check the experiment remainining data packages */
+				if (data_packages_count > 0)
+				{
+					log_print_event("Reading data packages");
+					
+					/* Read the packages with a maximum of "MAX_PAYLOAD_DATA_PACKAGES" packages */
+					if (data_packages_count < MAX_PAYLOAD_DATA_PACKAGES)
+					{
+						for (int i = 0; i < data_packages_count; i++)
+						{
+							harsh_get_data(data);
+							store_payload_data(data);
+						}
+					}
+					else 
+					{
+						for (int i = 0; i < MAX_PAYLOAD_DATA_PACKAGES; i++)
+						{
+							harsh_get_data(data);
+							store_payload_data(data);
+						}
+					}
+				}
+			}
+			else 
+			{
+				log_print_event("Error detected during get state routine!");
+			}
+
+			delay_ms(EXPERIMENT_ROUTINE_PERIOD);
+		}
+
+
+		delay_ms(EXPERIMENT_SECTION_PERIOD);
+	}
+}
 
 /** \} End of main group */
